a6a6a3d1e8c91277f684469e48ac740cc6adafdc
Insu-Lee <insu.lee@mrhi.or.kr>
2025-04-17 17:00:18 +0900

    fix: hardhat.config

--- contract/hardhat.config.ts
+++ contract/hardhat.config.ts
@@ -10,10 +10,6 @@ const config: HardhatUserConfig = {
   solidity: '0.8.28',
   networks: {
     // Todo: 원하는 네트워크를 선택하여 설정합니다.
-    kairos: {
-      url: 'https://public-en-kairos.node.kaia.io',
-      accounts: [privateKey || ''],
-    },
   },
 };
 
